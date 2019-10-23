d04a03679b7644ec80066bb360b0d33788f9502a
Harry Liu <byliuyang11@gmail.com>
2019-10-22 23:37:38 -0700

    Add AllocateKeys RPC call

--- app/adapter/rpc/kgs.go
+++ app/adapter/rpc/kgs.go
@@ -2,15 +2,28 @@ package rpc
 
 import (
 	"context"
-
-	"github.com/byliuyang/kgs/app/usecase/keys/producer"
+	"github.com/byliuyang/app/fw"
+	"github.com/byliuyang/kgs/app/usecase/keys"
 	"github.com/golang/protobuf/ptypes/empty"
 )
 
 var _ KeyGenServer = (*KeyGenController)(nil)
 
 type KeyGenController struct {
-	producer producer.Producer
+	producer keys.Producer
+	consumer keys.Consumer
+	logger fw.Logger
+}
+
+func (k KeyGenController) AllocateKeys(
+	ctx context.Context,
+	req *AllocateKeysRequest,
+) (*AllocateKeysResponse, error) {
+	allocatedKeys, err := k.consumer.ConsumeInBatch(uint(req.MaxKeyCount))
+	if err != nil {
+		return &AllocateKeysResponse{}, err
+	}
+	return &AllocateKeysResponse{Keys: allocatedKeys}, nil
 }
 
 func (k KeyGenController) PopulateKeys(
@@ -18,13 +31,22 @@ func (k KeyGenController) PopulateKeys(
 	req *PopulateKeysRequest,
 ) (*empty.Empty, error) {
 	go func() {
-		k.producer.Produce(uint(req.KeySize))
+		err := k.producer.Produce(uint(req.KeyLength))
+		if err != nil {
+			k.logger.Error(err)
+		}
 	}()
 	return &empty.Empty{}, nil
 }
 
-func NewKeyGenController(producer producer.Producer) KeyGenController {
+func NewKeyGenController(
+	producer keys.Producer,
+	consumer keys.Consumer,
+	logger fw.Logger,
+) KeyGenController {
 	return KeyGenController{
 		producer: producer,
+		consumer: consumer,
+		logger: logger,
 	}
 }
